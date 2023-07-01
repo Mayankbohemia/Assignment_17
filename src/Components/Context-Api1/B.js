f9916f6c58fe2acaf6081774e514591f6224a9aa
Mayankbohemia <b160024@nitsikkim.ac.in>
2023-07-02 00:26:38 +0530

    first commit

---
+++ src/Components/Context-Api1/B.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import C from './C';
+
+const A = () => {
+  
+  return (
+    <div>
+        <h2>B components</h2>
+       <C/>
+    
+    </div>
+  )
+}
+
+export default A
